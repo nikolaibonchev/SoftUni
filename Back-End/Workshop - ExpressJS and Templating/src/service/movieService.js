dc55824c740a223cf823ea71e060392863abfd7f
Nikolay Bonchev <nikieew@gmail.com>
2025-08-07 16:58:18 +0300

    Finish Search

--- Back-End/Workshop - ExpressJS and Templating/src/service/movieService.js
+++ Back-End/Workshop - ExpressJS and Templating/src/service/movieService.js
@@ -42,6 +42,15 @@ export default {
       if (filter.search){
         result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
       }
+
+      if (filter.genre){
+        result = result.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase());
+      }
+
+      if(filter.year){
+        result = result.filter(movie => movie.year === Number(filter.year));
+      }
+
       return result;
     },
     create(movieData) {
